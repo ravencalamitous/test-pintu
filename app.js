4dfc8cb2acd12461b0bfcab4338d4a342ebb6159
Raven <raventheclaw@yahoo.com>
2024-05-02 12:12:28 +0000

    add changes code for output

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-const http = require('http');
+console.log('Hello Test Pintu!')
 
-const hostname = '0.0.0.0';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var moment = require('moment');
+var date = moment().format('LL');
+console.log(date);
